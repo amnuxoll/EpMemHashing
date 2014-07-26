83470c786a59aaad1c01ebe785f908c49c3901ec
meyer14 <meyer14@EN304-15828.campus.up.edu>
2014-07-26 09:40:49 -0700

    Add javaDoc comments to all methods.
    Added the comments to allow for easier creation of unit tests.

--- hashTesting/src/comparisons/EpisodeCache.java
+++ hashTesting/src/comparisons/EpisodeCache.java
@@ -2,22 +2,92 @@ package comparisons;
 
 import hashTesting.WME;
 
+/**
+ * EpisodeCache class which is used to store and look up reference episodes for usage in memory recall
+ * and reconstruction.
+ * 
+ * @author Andrew Meyer
+ * @author Kevin Bastien
+ * @version July 26th, 2014
+ */
+
 public class EpisodeCache {
 	
-	public EpisodeCache (int size){
-		
+	private WME[][] cache;
+	
+	/**
+	 * Ctor for an EpisodeCache which contains a set of epsiodes that are used for reference.
+	 * 
+	 * @param sizeOfCache the maximum number of episodes that can be contained within the cache.
+	 */
+	public EpisodeCache (int sizeOfCache){
+		cache = new WME[sizeOfCache][];
 	}
 	
-	public WME[] addEpisode(WME[] newEpisode){
+	/**
+	 * Add an episode to the cache and return the removed episode if one exists.
+	 * 
+	 * @param newEpisode The new episode to be added into the cache. Unless this episode is completely
+	 * 		  contained within another episode it will always exist in the cache at the end of the call.
+	 * @return The episode that was removed from the cache. Null if no episode was removed (cache is not full).
+	 */
+	public WME[] addEpisode(WME[] newEpisode){		
 		return null;
 	}
 	
+	
+	/**
+	 * Add an episode to the cache at the specified index and return the removed episode if one exists.
+	 * 
+	 * @param newEpisode The new episode to be added to the cache.
+	 * @param index The index at which the episode will be added.
+	 * @return The episode that was removed from the cache. Null if no episode was removed (no episode at that index).
+	 */
+	public WME[] addEpisode(WME[] newEpisode, int index){
+		return null;
+	}
+	
+	/**
+	 * Remove an episode from the cache at the specified index.
+	 * 
+	 * @param index The index at which to remove the episode.
+	 * @return The episode that has been removed.
+	 */
+	public WME[] removeEpisode(int index){
+		return null;
+	}
+	
+	/**
+	 * Find the best matching episode and return it to the calling function.
+	 * 
+	 * @param cue The reference that is used to find the most appropriate episode.
+	 * @return The best matching episode.
+	 */
 	public WME[] findBestMatch(WME[] cue){
 		return null;
 	}
 	
+	/**
+	 * Get the episode at the given index in the cache.
+	 * 
+	 * @param index The index of the episode to be returned.
+	 * @return The episode at the specified index. 
+	 */
 	public WME[] get(int index){
-		return null;
+		return cache[index];
+	}
+	
+	/**
+	 * Compares two episodes based on how many unique WMEs one has in comparison to the other. 
+	 * Ordering does matter, wmeCompare(ep1, ep2) does not necessarily equal wmeCompare(ep2, ep1)
+	 * 
+	 * @param ep1 The episode that is being compared.
+	 * @param ep2 The episode being compared against.
+	 * @return The uniqueness score, the lower the number the less unique the first episode is in comparison to
+	 * 		   the second episode.
+	 */
+	private int wmeCompare(WME[] ep1, WME[] ep2){
+		return Integer.MAX_VALUE;
 	}
 	
 
--- hashTesting/src/comparisons/HashCodeList.java
+++ hashTesting/src/comparisons/HashCodeList.java
@@ -3,6 +3,15 @@ package comparisons;
 import hashTesting.HashFn;
 import hashTesting.WME;
 
+/**
+ * HashCode class which is used to store and look up hash codes for usage in memory recall
+ * and reconstruction. Used in conjunction with EpisodeCache.
+ * 
+ * @author Andrew Meyer
+ * @author Kevin Bastien
+ * @version July 26th, 2014
+ */
+
 public class HashCodeList {
 
 	private int[][] hashCodes;
@@ -10,24 +19,65 @@ public class HashCodeList {
 	private EpisodeCache cache;
 	private HashFn fn;
 	
+	/**
+	 * Ctor for the HashCodeList which is a list of hash codes that are used
+	 * in memory retrieval and reconstruction.
+	 * 
+	 * @param maxSize Max number of hash codes in list.
+	 */
 	public HashCodeList(int maxSize){
 		
 	}
 	
-	public int[] addCode(int[] code, int index){
+	/**
+	 * Add a hash code to the list and return the removed code if one exists
+	 * 
+	 * @param code The new code to be added to the cache.
+	 * @return The hash code that was removed.
+	 */
+	public int[] addCode(int[] code){
 		return null;
 	}
 	
+	/**
+	 * Find the best matching hash code and return it to the calling function
+	 * 
+	 * @param code The reference that is used to find the most appropriate code.
+	 * @return The best matching hash code.
+	 */
 	public int[] findBestMatch(int[] code){
 		return null;
 	}
 	
+	/**
+	 * Get the hash code at the given index.
+	 * 
+	 * @param index The index of the code to be returned.
+	 * @return The hash code at the given index.
+	 */
 	public int[] get(int index){
 		return null;
 	}
 	
+	/**
+	 * 
+	 * @param cue
+	 * @return
+	 */
 	public WME[] recunstructEpisode(int[] cue){
 		return null;
+	}
+	
+	/**
+	 * Compares two hash codes based upon their difference in bits (XORed and then finding the number of bits that are still '1').
+	 * Ordering does not matter, hashCompare(code1, code2) will always equal hashCompare(code2, code1).
+	 * 
+	 * @param code1 The first code being compared
+	 * @param code2 The second code being compared
+	 * @return The uniqueness score, the lower the number the less unique the episodes are in relation to each other.
+	 */
+	private int hashCompare(int[] code1, int[] code2){
+		return Integer.MAX_VALUE;
 	}
 	
 }
